b49f130ff075e6ed1aa9a3dea123f2f32d03abc5
Arina <arinaligit@gmail.com>
2024-01-05 21:59:14 +0300

    add new solution 442

--- 442.Find_All_Duplicates_in_an_Array/main.go
+++ 442.Find_All_Duplicates_in_an_Array/main.go
@@ -9,6 +9,9 @@ var nums = []int{4, 3, 2, 7, 8, 2, 3, 1}
 func main() {
 	res := findDuplicates(nums)
 	fmt.Println(res)
+
+	res2 := findDuplicates2(nums)
+	fmt.Println(res2)
 }
 
 func findDuplicates(nums []int) []int {
@@ -23,6 +26,20 @@ func findDuplicates(nums []int) []int {
 		if v == 2 {
 			res = append(res, k)
 		}
+	}
+
+	return res
+}
+
+func findDuplicates2(nums []int) []int {
+	m := make(map[int]struct{})
+	res := []int{}
+
+	for _, num := range nums {
+		if _, ok := m[num]; ok {
+			res = append(res, num)
+		}
+		m[num] = struct{}{}
 	}
 
 	return res
