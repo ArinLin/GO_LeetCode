61aed85d04b774155bac4db76394cac77503e48e
Arina <arinaligit@gmail.com>
2024-02-05 15:38:38 +0300

    add pointers task

--- pointers_tasks/main.go
+++ pointers_tasks/main.go
@@ -5,6 +5,7 @@ import (
 )
 
 func main() {
+	// first Task
 	var numbers []*int
 	for _, value := range []int{1, 2, 3, 4} {
 		numbers = append(numbers, &value)
@@ -13,4 +14,11 @@ func main() {
 	for _, number := range numbers {
 		fmt.Printf("%d ", *number) // Output 4 4 4 4
 	}
+
+	// second Task
+	x := 3
+	y := &x
+	fmt.Print(*y) // Output 3
+	*y = 4
+	fmt.Println(x) // Output 4
 }
